fa59f221701bc05c4dda4c3fef3e7619a3b54bf1
maximus9802 <vuquy090802@gmail.com>
2023-09-12 21:32:42 +0700

    add discovery server

--- inventoryservice/src/main/java/com/quyvx/inventoryservice/InventoryServiceApplication.java
+++ inventoryservice/src/main/java/com/quyvx/inventoryservice/InventoryServiceApplication.java
@@ -5,8 +5,9 @@ import com.quyvx.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventoryServiceApplication {
 
